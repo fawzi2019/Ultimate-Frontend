4cd6f00c1c98d1eeaf6cd877cdf386adc5eaa231
fawzi2019 <astafanousf@gmail.com>
2021-01-26 17:09:57 +0200

    Lesson 11

--- gulpfile.js
+++ gulpfile.js
@@ -36,7 +36,7 @@ gulp.task('html', function () {
 gulp.task('scss', function () {
   return gulp.src(['stage/css/**/*.css', 'stage/css/**/*.scss'])
        .pipe(sourcemaps.init())
-  		 .pipe(sass(/*{outputStyle: 'compressed'}*/).on('error', sass.logError))
+  		 .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
   		 .pipe(prefix('last 2 versions'))
   		 .pipe(concat('main.css'))
        .pipe(sourcemaps.write('.'))
