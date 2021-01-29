0186af4eaa33690781ab19f8de74d9d45151b77d
fawzi2019 <astafanousf@gmail.com>
2021-01-29 13:59:16 +0200

    Lesson 12, 13, 14

--- dist/js/main.js
+++ dist/js/main.js
@@ -3,6 +3,16 @@ $(function() {
     $('.toggle-sidebar').on('click', function () {
         $('.content-area, .sidebar ').toggleClass('no-sidebar');
     });
+    // Toggle-Submenu
+    $('.toggle-submenu').on('click', function () {
+        //  $(this).toggleClass('fa-angle-right fa-angle-down');
+        $(this).find('.fa-angle-right')
+        .toggleClass("down");
+        $(this)
+        //   .parent("a")
+          .next(".child-links")
+          .slideToggle();
+    });
 });
 const myMap = new Map();
 const mySet = new Set();;
--- stage/js/jeast.js
+++ stage/js/jeast.js
@@ -3,4 +3,14 @@ $(function() {
     $('.toggle-sidebar').on('click', function () {
         $('.content-area, .sidebar ').toggleClass('no-sidebar');
     });
+    // Toggle-Submenu
+    $('.toggle-submenu').on('click', function () {
+        //  $(this).toggleClass('fa-angle-right fa-angle-down');
+        $(this).find('.fa-angle-right')
+        .toggleClass("down");
+        $(this)
+        //   .parent("a")
+          .next(".child-links")
+          .slideToggle();
+    });
 });
