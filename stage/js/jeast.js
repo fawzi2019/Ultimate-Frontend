53f5ddea64cf499c3621390831f476cfb33bff6e
fawzi2019 <astafanousf@gmail.com>
2021-01-30 04:27:39 +0200

    Lesson 18

--- stage/js/jeast.js
+++ stage/js/jeast.js
@@ -13,4 +13,44 @@ $(function() {
           .next(".child-links")
           .slideToggle();
     });
+    // Open / Close FullScreen
+    $('.toggle-fullscreen').on('click', function () {
+      $(this).toggleClass('full-screen');
+      if ($(this).hasClass('full-screen')) { /* Page Is Now FullScreen*/
+        openFullscreen();
+      } else { /* Page Is Now Not FullScreen*/
+        closeFullscreen();
+      }
+    });
 });
+
+
+
+/* Get The documentElement <html> to display the page in fullscreen*/
+var elem = document.documentElement;
+
+/*View in fullscreen */
+function openFullscreen() {
+    if (elem.requestFullscreen) {
+        elem.requestFullscreen();
+    } else if (elem.mozRequestFullScreen) { /*Firefox*/
+       elem.mozRequestFullScreen();
+    } else if (elem.webkitRequestFullscreen) { /*Chrome, Safari and Opera */
+       elem.webkitRequestFullscreen();
+    } else if (msRequestFullscreen) { /* IE/Edge */
+       elem.msRequestFullscreen();
+    }
+}
+
+/*View in fullscreen */
+function closeFullscreen() {
+    if (document.exitFullscreen) {
+        document.exitFullscreen();
+    } else if (document.mozCancelFullScreen) { /* Firefox */
+      document.mozCancelFullScreen();
+    } else if (document.webkitExitFullScreen) { /* Chrome, Safari and Opera */
+       document.webkitExitFullScreen();
+    } else if (document.msExitFullScreen) {/* IE/Edge */
+       document.msExitFullScreen();
+    }
+}
